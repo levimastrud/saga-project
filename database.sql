edc0829c1178ef1120050cfdbf829b96068608f8
Levi <levikeithmastrud@gmail.com>
2022-06-09 11:15:59 -0500

    npm installed

--- database.sql
+++ database.sql
@@ -3,6 +3,13 @@ CREATE DATABASE "giphy_search_favorites";
 -- You'll need a table for storing each giphy image favorite
 -- Each favorite image can be assigned 1 of the following categories as a Foreign Key
 
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "url" VARCHAR (500) NOT NULL,
+    "search" VARCHAR (100) NOT NULL,
+    "category" 
+)
+
 -- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
