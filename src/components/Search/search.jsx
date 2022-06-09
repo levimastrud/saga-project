53cb3c176e7f9f9f4ab5be4f14c447d74b2fd7f4
Levi <levikeithmastrud@gmail.com>
2022-06-09 15:53:37 -0500

    added search clear

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -5,25 +5,26 @@ import { useHistory } from 'react-router-dom';
 
 
 function Search() {
-    //const dispatch = useDispatch();
-    //const results = useSelector(store => store.search)
-    let [searchInput, setSearchInput] = useState();
+    const dispatch = useDispatch();
+    const results = useSelector(store => store.search)
+    let [searchInput, setSearchInput] = useState('');
     let imgUrl = ``;
 
     function searchButton() {
-        imgUrl = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}=${search}&limit=25&offset=0&rating=pg-13&lang=en`;
-
-        //dispatch({ type: "SEARCH", payload: url})
+        imgUrl = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}=${searchInput}&limit=25&offset=0&rating=pg-13&lang=en`;
+        dispatch({ type: "SEARCH", payload: searchInput })
+        setSearchInput('');
     }
 
     return (
         <>
             <div>
+                <h1>Search</h1>
                 <input value={searchInput} type="text" onChange={(event) => setSearchInput(event.target.value)} />
                 <button id="search" onClick={() => searchButton()}>Search</button>
             </div>
             <div>
-                <img source={imgUrl} alt="Giphy" style={{height: '600px', maxWidth: '600px'}}/>
+                <img source={imgUrl} alt="Giphy" style={{ height: '600px', maxWidth: '600px' }} />
             </div>
         </>
     )
