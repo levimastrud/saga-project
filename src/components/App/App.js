d3cfe561c606c722397198c60f286bb7790cab55
Brian Campbell <bry123ftw@gmail.com>
2022-06-09 11:48:50 -0500

    added search component and sourced into app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Search from '../Search/search';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <Search />
     </div>
   );
 }
