b0d6b316050d24c41ee86e4bee0522a5e959c124
Levi <levikeithmastrud@gmail.com>
2022-06-09 11:22:53 -0500

    added dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import App from './components/App/App.js';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
 
 ReactDOM.render(<App />, document.getElementById('root'));
