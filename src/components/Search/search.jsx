bf0fe12fb58663521a0878b8385ce980d85faf70
Brian Campbell <bry123ftw@gmail.com>
2022-06-09 16:42:56 -0500

    post pull commit

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -5,26 +5,27 @@ import { useHistory } from 'react-router-dom';
 
 
 function Search() {
-    const dispatch = useDispatch();
-    const results = useSelector(store => store.search)
-    let [searchInput, setSearchInput] = useState('');
+    //const dispatch = useDispatch();
+    //const results = useSelector(store => store.search)
+    let [searchInput, setSearchInput] = useState();
     let imgUrl = ``;
     let img2Url = '';
 
     function searchButton() {
-
         console.log('search button', searchInput);
+       
+        
+        //dispatch({ type: "SEARCH", payload: url})
     }
 
     return (
         <>
             <div>
-                <h1>Search</h1>
                 <input value={searchInput} type="text" onChange={(event) => setSearchInput(event.target.value)} />
                 <button id="search" onClick={() => searchButton()}>Search</button>
             </div>
             <div>
-                <img source={imgUrl} alt="Giphy" style={{ height: '600px', maxWidth: '600px' }} />
+                <img source={img2Url} alt="Giphy" style={{ height: '600px', maxWidth: '600px' }} />
             </div>
         </>
     )
