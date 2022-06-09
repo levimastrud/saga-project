9eca2174263bff6a41d9505c0b9fee1e46ac045d
Brian Campbell <bry123ftw@gmail.com>
2022-06-09 12:03:18 -0500

    added search component, frame is somewhat setup

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -1,6 +1,27 @@
+import axios from "axios";
+import React, {useEffect, useState } from "react";
+import {useSelector, useDispatch} from "react-redux"
+import { useHistory } from 'react-router-dom';
+
 
 function Search() {
+const dispatch = useDispatch();
+const results = useSelector(store => store.search)
+let [searchInput, setSearchInput] =useState();
+
+function searchButton(){
+
+    dispatch({ type: "SEARCH", payload: url})
+}
+
+    return(
+        <div>
+            <input value={searchInput} type="text" onChange={(event) => setSearchInput(event.target.value)} />
+            <button id="search" onClick={() => searchButton()}>Search</button>
+        </div>
+    )
 
 }
+
 
 export default Search;
