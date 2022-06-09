c25b566e278e78bff636cebc74843eca7f8d7e68
Brian Campbell <bry123ftw@gmail.com>
2022-06-09 11:28:11 -0500

    added todo notes to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,3 +9,12 @@ function App(props) {
 }
 
 export default App;
+
+/* 
+  NOTES 
+-search will look like this: https://api.giphy.com/v1/gifs/search?api_key={YOUR API KEY HERE}={SEARCH CRITERIA HERE}&limit=25&offset=0&rating=pg-13&lang=en
+-database will need:
+  -id, url, foriegn key from category_id
+- when image is favorited we will need to store URL and have the user select a category it falls under to send to database for storage
+- 
+*/
