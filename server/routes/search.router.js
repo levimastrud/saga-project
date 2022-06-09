b51864abde5fd9b60f8fd3d6527307388d64f798
Brian Campbell <bry123ftw@gmail.com>
2022-06-09 16:35:47 -0500

    post request to start get request for Giphy works

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -1,13 +1,16 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const pool = require('../modules/pool');
+require('dotenv').config();
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log(req);
-    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${search}&limit=25&offset=0&rating=pg-13&lang=en`).then((response) => {
-        res.send(response.data);
-}).catch(err => {
+router.post('/', (req, res) => {
+    console.log(req.body);
+    console.log(req.body.search);
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.body.search}&limit=5&offset=0&rating=pg-13&lang=en`)
+    .then((response) => res.send(response.data))
+    .catch(err => {
         console.log(err);
         res.sendStatus(500);
     });
