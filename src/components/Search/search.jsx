632eaf10c771d7caa7ded8049455eab9f32fe41c
Levi <levikeithmastrud@gmail.com>
2022-06-10 11:13:24 -0500

    messed around with stars

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux"
 import { useHistory } from 'react-router-dom';
+import star from './star.svg'
+import hollowStar from './hollow_star.svg'
 
 
 function Search() {
@@ -9,7 +11,6 @@ function Search() {
     const results = useSelector(store => store.search)
     let [searchInput, setSearchInput] = useState('');
     let imgUrl = ``;
-    let img2Url = '';
 
     function searchButton() {
 
@@ -23,6 +24,8 @@ function Search() {
                 <input value={searchInput} type="text" onChange={(event) => setSearchInput(event.target.value)} />
                 <button id="search" onClick={() => searchButton()}>Search</button>
             </div>
+            <img  className = 'filledStar' src={star}/>
+            <img  className = 'hollowStar' src={hollowStar}/>
             <div>
                 <img source={imgUrl} alt="Giphy" style={{ height: '600px', maxWidth: '600px' }} />
             </div>
