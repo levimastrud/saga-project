625adc85b4b7122cb892daf8be7dbb4067dc15a0
Brian Campbell <bry123ftw@gmail.com>
2022-06-10 11:13:05 -0500

    search works

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -5,29 +5,38 @@ import { useHistory } from 'react-router-dom';
 
 
 function Search() {
-    //const dispatch = useDispatch();
-    //const results = useSelector(store => store.search)
-    let [searchInput, setSearchInput] = useState();
-    let imgUrl = ``;
-    let img2Url = '';
+    const dispatch = useDispatch();
+    let results = results = useSelector(store => store.search)
+    let [searchInput, setSearchInput] = useState("");
+    let imgUrl = [];
+
+    console.log('results are ', results);
+    
+
+    useEffect(() => {
+    }, [])
+
+    
 
     function searchButton() {
         console.log('search button', searchInput);
-       
-        
-        //dispatch({ type: "SEARCH", payload: url})
+
+
+        dispatch({ type: "SEARCH", payload: searchInput })
     }
 
     return (
-        <>
+        < div id="gallery">
             <div>
                 <input value={searchInput} type="text" onChange={(event) => setSearchInput(event.target.value)} />
                 <button id="search" onClick={() => searchButton()}>Search</button>
             </div>
-            <div>
-                <img source={img2Url} alt="Giphy" style={{ height: '600px', maxWidth: '600px' }} />
+               {results ? results.map((image,i) => (
+            <div key={i}>
+                <img src={image.images.original.url} alt="Giphy" style={{ height: '600px', maxWidth: '600px' }} />
             </div>
-        </>
+               )  ): ''}
+        </div>
     )
 
 }
